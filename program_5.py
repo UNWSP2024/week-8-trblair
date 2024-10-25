90eb331f0763e559036034a90581e807b99fe0e6
trblair <trblair@students.unwsp.edu>
2024-10-25 18:44:42 -0500

    Update program_5.py

--- program_5.py
+++ program_5.py
@@ -3,3 +3,17 @@
 # For example a course ID could be "COS 2005" and the course name could be "Python Programming."   
 # Then ask the user for a subject (like "COS"). 
 # Finally, the program will display the ID and name of all the courses having that subject.
+def courseslection():
+    x=1
+    classlist=[]
+    print("Make sure that when you're inputting course IDs, there is a space between the subject and the ID (eg. COS 2005, not COS2005)")
+    while x>0:
+        print("Please input a course ID")
+        classlist.append(input())
+        again=input("Would you like to enter another course id? y/n\n")
+        if again=="n":
+            x=0
+    subject=input("What subject would you like to search for?\n")
+    l1 = [i for i in classlist if subject in i]
+    print(l1)
+courseslection()
